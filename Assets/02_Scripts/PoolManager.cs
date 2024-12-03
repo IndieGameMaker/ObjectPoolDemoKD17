5058dc73ad04dcd371024b776c639dd0e519762c
LeeJaeHyun <myleje@gmail.com>
2024-12-03 15:13:40 +0900

    .

--- Assets/02_Scripts/PoolManager.cs
+++ Assets/02_Scripts/PoolManager.cs
@@ -8,5 +8,16 @@ public class PoolManager : MonoBehaviour
 
     [SerializeField] private GameObject bulletPrefab;
 
-
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
