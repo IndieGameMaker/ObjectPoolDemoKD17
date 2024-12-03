da30e35cc36c4436177b3890c7ad7ec1a11298d3
LeeJaeHyun <myleje@gmail.com>
2024-12-03 15:22:50 +0900

    .

--- Assets/02_Scripts/PoolManager.cs
+++ Assets/02_Scripts/PoolManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -19,5 +20,39 @@ public class PoolManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    void Start()
+    {
+        // ObjectPool 초기화
+        bulletPool = new ObjectPool<Bullet>(
+            createFunc: CreateItem,
+            actionOnGet: OnTakeItem,
+            actionOnRelease: OnReturnItem,
+            actionOnDestroy: OnDestroyItem,
+            defaultCapacity: 10,
+            maxSize: 20
+        );
+    }
+
+    private Bullet CreateItem()
+    {
+        return Instantiate(bulletPrefab).GetComponent<Bullet>();
+    }
+
+    private void OnTakeItem(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(true);
+    }
+
+    private void OnReturnItem(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyItem(Bullet bullet)
+    {
+        Debug.Log("삭제됨");
+        Destroy(bullet.gameObject);
     }
 }
