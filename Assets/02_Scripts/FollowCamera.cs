eaa148b8f6337efabfb3bd5cec7cbf4b067373a8
LeeJaeHyun <myleje@gmail.com>
2024-12-03 14:41:10 +0900

    .

--- Assets/02_Scripts/FollowCamera.cs
+++ Assets/02_Scripts/FollowCamera.cs
@@ -7,11 +7,16 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] private float height = 3.0f;
     [SerializeField] private float damping = 1.0f;
 
+    [Range(0.0f, 5.0f)]
+    public float lookAtPoint = 0.0f;
+
     void LateUpdate()
     {
         Vector3 pos = target.position - (target.forward * distance) + (Vector3.up * height);
 
         transform.position = Vector3.Slerp(transform.position, pos, Time.deltaTime * damping);
-        transform.LookAt(target);
+
+        Vector3 _target = target.position + (Vector3.up * lookAtPoint);
+        transform.LookAt(_target);
     }
 }
